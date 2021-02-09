39ed8d97561df3aefe576ed5f7a4cc66a9432c56
Rehan Khalil <rehankhalil462@gmail.com>
2021-02-10 02:44:08 +0500

    Added react-router-dom, react-router-bootstrap,react-bootstrap, styled-components,sweetalert2 libraries. Created HomePage, Product Page, Header,Footer,Rating,Product components. Used Bootswatch lux theme. Implemented nested React-Router paths. Designed product details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
+import HomePage from './pages/HomePage/HomePage';
+import ProductPage from './pages/ProductPage/ProductPage';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/product/:id' component={ProductPage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
