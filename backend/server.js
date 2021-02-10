352416521cafd4fd76b642a3cdcfbfa649349dbc
Rehan Khalil <rehankhalil462@gmail.com>
2021-02-10 19:12:04 +0500

    made cluster and then database on MongoDB and then connected our project with MongoDB using conncection string through mongoose package  and installed colors package to just make some colors for consoles like when MongoDB is connected or authenticated or failed, and if server is running and stuff like that

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import colors from 'colors';
 import dotenv from 'dotenv';
 import products from './data/products.js';
+import connectDB from './config/db.js';
+
 dotenv.config();
+connectDB();
 //initializing express server
 const app = express();
 
@@ -24,5 +28,7 @@ const PORT = process.env.PORT || 5000;
 //backend server will serve on this port
 app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
