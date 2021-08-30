32d926d6249e1c326f5fd30dac01e9f7e3eff8a0
Rehan Khalil <rehankhalil462@gmail.com>
2021-08-30 12:51:57 +0500

    revamping

--- backend/server.js
+++ backend/server.js
@@ -36,9 +36,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
 } else {
   //what to do when someone asks or target homepage or '/' route
   app.get('/', (req, res) => {
